e3469321047df20519c785a6d1f0b6f6947b5b08
Manoel Vidal <manoel.vidal@opencartbrasil.com.br>
2014-10-06 06:31:54 -0300

    Tradução do e-mail de recuperação de senha da administração.

--- admin/language/portuguese-br/mail/forgotten.php
+++ admin/language/portuguese-br/mail/forgotten.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_subject']  = '%s - Password reset request';
-$_['text_greeting'] = 'A new password was requested for %s administration.';
-$_['text_change']   = 'To reset your password click on the link below:';
-$_['text_ip']       = 'The IP used to make this request was: %s';
+$_['text_subject']  = '%s - Solicitação de redefinição de senha';
+$_['text_greeting'] = 'Foi solicitada a redefinição de senha para administração da loja %s.';
+$_['text_change']   = 'Para recadastrar sua senha, clique no link abaixo:';
+$_['text_ip']       = 'O IP utilizado para fazer esta solicitação foi: %s';
