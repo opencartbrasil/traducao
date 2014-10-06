f1869c02f6440eee1cad379e7a1f66d463e74a56
Manoel Vidal <manoel.vidal@opencartbrasil.com.br>
2014-10-06 04:52:20 -0300

    Atualização ebay

--- catalog/language/portuguese-br/ebay/order.php
+++ catalog/language/portuguese-br/ebay/order.php
@@ -1,8 +1,8 @@
 <?php
 //Order totals
-$_['lang_shipping']     = 'Shipping';
-$_['lang_discount']     = 'Discount';
-$_['lang_tax']          = 'Tax';
+$_['lang_shipping']     = 'Frete';
+$_['lang_discount']     = 'Desconto';
+$_['lang_tax']          = 'Imposto';
 $_['lang_subtotal']     = 'Sub-total';
 $_['lang_total']        = 'Total';
 ?>
