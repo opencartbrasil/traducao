1367a0f13ecd1640a87324b38fafc538dccb0ef9
Crock-Linux <robsonwr@gmail.com>
2014-10-28 22:11:27 -0200

    Update cart.php

--- catalog/language/portuguese-br/common/cart.php
+++ catalog/language/portuguese-br/common/cart.php
@@ -1,7 +1,7 @@
 <?php
 // Text
 $_['text_items']    = '%s item(s) - %s';
-$_['text_empty']    = 'O seu carrinho de compras está vazio!;
+$_['text_empty']    = 'O seu carrinho de compras está vazio!';
 $_['text_cart']     = 'Ver carrinho';
 $_['text_checkout'] = 'Finalizar pedido';
 $_['text_recurring']  = 'Perfil de pagamento';
