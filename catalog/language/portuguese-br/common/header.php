23588bb3dc330e995fc2371f4b2570067943407d
Crock-Linux <robsonwr@gmail.com>
2014-10-29 13:35:20 -0200

    Update header.php

--- catalog/language/portuguese-br/common/header.php
+++ catalog/language/portuguese-br/common/header.php
@@ -1,16 +1,16 @@
 <?php
 // Text
-$_['text_home']          = 'Home';
-$_['text_wishlist']      = 'Wish List (%s)';
-$_['text_shopping_cart'] = 'Shopping Cart';
-$_['text_category']      = 'Categories';
-$_['text_account']       = 'My Account';
-$_['text_register']      = 'Register';
-$_['text_login']         = 'Login';
-$_['text_order']         = 'Order History';
-$_['text_transaction']   = 'Transactions';
+$_['text_home']          = 'Principal';
+$_['text_wishlist']      = 'Lista de Desejos (%s)';
+$_['text_shopping_cart'] = 'Meu Carrinho';
+$_['text_category']      = 'Categorias';
+$_['text_account']       = 'Minha Conta';
+$_['text_register']      = 'Registrar';
+$_['text_login']         = 'Acessar';
+$_['text_order']         = 'Histórico de Pedidos';
+$_['text_transaction']   = 'Crédito e Débito';
 $_['text_download']      = 'Downloads';
-$_['text_logout']        = 'Logout';
-$_['text_checkout']      = 'Checkout';
-$_['text_search']        = 'Search';
-$_['text_all']           = 'See All';
+$_['text_logout']        = 'Sair';
+$_['text_checkout']      = 'Finalizar Pedido';
+$_['text_search']        = 'Busca';
+$_['text_all']           = 'Ver Todos';
