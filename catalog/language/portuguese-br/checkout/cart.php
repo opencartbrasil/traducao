752481e489c278aba9b0778b1d8bdde158f8453b
Crock-Linux <robsonwr@gmail.com>
2014-10-29 13:54:04 -0200

    Update cart.php

--- catalog/language/portuguese-br/checkout/cart.php
+++ catalog/language/portuguese-br/checkout/cart.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']    = 'Shopping Cart';
+$_['heading_title']    = 'Meu Carrinho';
 
 // Text
-$_['text_success']     = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">shopping cart</a>!';
-$_['text_remove']      = 'Success: You have modified your shopping cart!';
-$_['text_login']       = 'Attention: You must <a href="%s">login</a> or <a href="%s">create an account</a> to view prices!';
+$_['text_success']     = 'Você adicionou <a href="%s">%s</a> ao seu <a href="%s">carrinho de compras</a>!';
+$_['text_remove']      = 'Você modificou seu carrinho de compras!';
+$_['text_login']       = 'Atenção: Acesse sua <a href="%s">conta</a> ou <a href="%s">cadastre-se</a> para visualizar os preços!';
 $_['text_items']       = '%s item(s) - %s';
-$_['text_points']      = 'Reward Points: %s';
-$_['text_next']        = 'What would you like to do next?';
-$_['text_next_choice'] = 'Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.';
-$_['text_empty']       = 'Your shopping cart is empty!';
-$_['text_day']         = 'day';
-$_['text_week']        = 'week';
-$_['text_semi_month']  = 'half-month';
-$_['text_month']       = 'month';
-$_['text_year']        = 'year';
+$_['text_points']      = 'Pontos de fidelidade: %s';
+$_['text_next']        = 'O que você gostaria de fazer em seguida?';
+$_['text_next_choice'] = 'Escolha se você tem um código de desconto ou pontos de fidelidade, ou se gostaria de calcular o custo do entrega.';
+$_['text_empty']       = 'Você ainda não adicionou produtos!';
+$_['text_day']         = 'dia';
+$_['text_week']        = 'semana';
+$_['text_semi_month']  = 'meio do mês';
+$_['text_month']       = 'mês';
+$_['text_year']        = 'ano';
 $_['text_trial']       = '%s every %s %s for %s payments then ';
 $_['text_recurring']   = '%s every %s %s';
 $_['text_length']      = ' for %s payments';
@@ -27,16 +27,16 @@ $_['text_payment_description'] 	              = '%s every %d %s(s) for %d paymen
 $_['text_payment_until_canceled_description'] = '%s every %d %s(s) until canceled';
 
 // Column
-$_['column_image']          = 'Image';
-$_['column_name']           = 'Product Name';
-$_['column_model']          = 'Model';
-$_['column_quantity']       = 'Quantity';
-$_['column_price']          = 'Unit Price';
+$_['column_image']          = 'Imagem';
+$_['column_name']           = 'Produto';
+$_['column_model']          = 'Modelo';
+$_['column_quantity']       = 'Quantidade';
+$_['column_price']          = 'Unitário';
 $_['column_total']          = 'Total';
 
 // Error
-$_['error_stock']            = 'Products marked with *** are not available in the desired quantity or not in stock!';
-$_['error_minimum']          = 'Minimum order amount for %s is %s!';
-$_['error_required']         = '%s required!';
-$_['error_product']          = 'Warning: There are no products in your cart!';
-$_['error_recurring_required'] = 'Please select a payment recurring!';
+$_['error_stock']            = 'Produtos marcados com *** não estão disponíveis na quantia desejada ou não encontram-se em estoque!';
+$_['error_minimum']          = 'A quantidade mínima para %s is %s!';
+$_['error_required']         = '%s é obrigatório!';
+$_['error_product']          = 'Atenção: Não há produtos em seu carrinho!';
+$_['error_recurring_required'] = 'Por favor selecione forma de pagamento!';
