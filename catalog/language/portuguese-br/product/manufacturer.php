d56d01de6d233dbb573d5dc5ac55817438361675
Crock-Linux <robsonwr@gmail.com>
2014-10-29 14:16:01 -0200

    Update manufacturer.php

--- catalog/language/portuguese-br/product/manufacturer.php
+++ catalog/language/portuguese-br/product/manufacturer.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Find Your Favorite Brand';
+$_['heading_title']     = 'Lista de Fabricantes';
 
 // Text
-$_['text_brand']        = 'Brand';
-$_['text_index']        = 'Brand Index:';
-$_['text_error']        = 'Brand not found!';
-$_['text_empty']        = 'There are no products to list.';
-$_['text_quantity']     = 'Qty:';
-$_['text_manufacturer'] = 'Brand:';
-$_['text_model']        = 'Product Code:';
-$_['text_points']       = 'Reward Points:';
-$_['text_price']        = 'Price:';
-$_['text_tax']          = 'Ex Tax:';
-$_['text_compare']      = 'Product Compare (%s)';
-$_['text_sort']         = 'Sort By:';
-$_['text_default']      = 'Default';
-$_['text_name_asc']     = 'Name (A - Z)';
-$_['text_name_desc']    = 'Name (Z - A)';
-$_['text_price_asc']    = 'Price (Low &gt; High)';
-$_['text_price_desc']   = 'Price (High &gt; Low)';
-$_['text_rating_asc']   = 'Rating (Lowest)';
-$_['text_rating_desc']  = 'Rating (Highest)';
-$_['text_model_asc']    = 'Model (A - Z)';
-$_['text_model_desc']   = 'Model (Z - A)';
-$_['text_limit']        = 'Show:';
+$_['text_brand']        = 'Fabricante';
+$_['text_index']        = 'Índice Alfabético por Letra:';
+$_['text_error']        = 'Fabricante não encontrado!';
+$_['text_empty']        = 'Não há produtos para listar.';
+$_['text_quantity']     = 'Qtd:';
+$_['text_manufacturer'] = 'Fabricante:';
+$_['text_model']        = 'Código do Produto:';
+$_['text_points']       = 'Pontos pela Compras:';
+$_['text_price']        = 'Preço:';
+$_['text_tax']          = 'Com Impostos:';
+$_['text_compare']      = 'Comparar Produtos (%s)';
+$_['text_sort']         = 'Ordenar por:';
+$_['text_default']      = 'Padrão';
+$_['text_name_asc']     = 'Nome (A - Z)';
+$_['text_name_desc']    = 'Nome (Z - A)';
+$_['text_price_asc']    = 'Preço (Menor &gt; Maior)';
+$_['text_price_desc']   = 'Preço (Maior &gt; Menor)';
+$_['text_rating_asc']   = 'Avaliação (Baixa)';
+$_['text_rating_desc']  = 'Avaliação (Alta)';
+$_['text_model_asc']    = 'Modelo (A - Z)';
+$_['text_model_desc']   = 'Modelo (Z - A)';
+$_['text_limit']        = 'Exibir até:';
