b46e5e04fd8d02e35c30280f40348d84ccd990b7
Crock-Linux <robsonwr@gmail.com>
2014-10-30 19:47:44 -0200

    Update store.php

--- catalog/language/portuguese-br/module/store.php
+++ catalog/language/portuguese-br/module/store.php
@@ -1,7 +1,7 @@
 <?php
 // Heading
-$_['heading_title'] = 'Choose a Store';
+$_['heading_title'] = 'Escolha uma loja';
 
 // Text
-$_['text_default']  = 'Default';
-$_['text_store']    = 'Please choose the store you wish to visit.';
+$_['text_default']  = 'Padrão';
+$_['text_store']    = 'Por favor, escolha a loja que deseja visitar.';
