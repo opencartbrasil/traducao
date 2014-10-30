1e7ae93a087c70ff07fcfb0fca41caeb1e33dfd4
Crock-Linux <robsonwr@gmail.com>
2014-10-30 19:07:00 -0200

    Update download.php

--- catalog/language/portuguese-br/account/download.php
+++ catalog/language/portuguese-br/account/download.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']     = 'Account Downloads';
+$_['heading_title']     = 'Conta Downloads';
 
 // Text
-$_['text_account']      = 'Account';
+$_['text_account']      = 'Conta';
 $_['text_downloads']    = 'Downloads';
-$_['text_empty']        = 'You have not made any previous downloadable orders!';
+$_['text_empty']        = 'Você não fez nenhum pedidos com downloads!';
 
 // Column
-$_['column_order_id']   = 'Order ID';
-$_['column_name']       = 'Name';
-$_['column_size']       = 'Size';
-$_['column_date_added'] = 'Date Added';
+$_['column_order_id']   = 'ID do Pedido';
+$_['column_name']       = 'Nome';
+$_['column_size']       = 'Tamanho';
+$_['column_date_added'] = 'Data de criação';
