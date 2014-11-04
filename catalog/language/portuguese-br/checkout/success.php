739cfd1d25edfd03290986b374eaaf62344f92d7
Eduardo Adriano Ramos <eduardo@openti.info>
2014-11-04 12:41:16 -0200

    catalog/checkout completo

--- catalog/language/portuguese-br/checkout/success.php
+++ catalog/language/portuguese-br/checkout/success.php
@@ -3,8 +3,8 @@
 $_['heading_title']        = 'Your order has been placed!';
 
 // Text
-$_['text_basket']          = 'Shopping Cart';
+$_['text_basket']          = 'Carrinho de Compras';
 $_['text_checkout']        = 'Checkout';
-$_['text_success']         = 'Success';
-$_['text_customer']        = '<p>Your order has been successfully processed!</p><p>You can view your order history by going to the <a href="%s">my account</a> page and by clicking on <a href="%s">history</a>.</p><p>If your purchase has an associated download, you can go to the account <a href="%s">downloads</a> page to view them.</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for shopping with us online!</p>';
-$_['text_guest']           = '<p>Your order has been successfully processed!</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for shopping with us online!</p>';
+$_['text_success']         = 'Successo';
+$_['text_customer']        = '<p>O seu pedido foi processado com sucesso!</p><p>Você pode ver o seu histórico de pedidos clicando em  <a href="%s">Minha Conta</a> ou <a href="%s">Histórico</a>.</p><p>Se a sua compra tem um download associado , você pode ir para a sua conta <a href="%s">Downloads</a> para visualizar-lo.</p><p>Por favor, envie quaisquer perguntas que você tem para o <a href="%s">proprietario da loja</a>.</p><p>Obrigado por comprar em nossa loja online!</p>';
+$_['text_guest']           = '<p>O seu pedido foi processado com sucesso!</p><p>Por favor, envie quaisquer perguntas que você tem para o <a href="%s">proprietario da loja</a>.</p><p>Obrigado por comprar em nossa loja online!</p>';
