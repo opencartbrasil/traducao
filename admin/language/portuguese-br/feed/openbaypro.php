75f0d9b8a7542915570b2a5dd2e1b1524c4bdd59
Eliton Torri <falecom@elitontorri.com.br>
2014-11-08 17:08:11 -0200

    Update openbaypro.php

--- admin/language/portuguese-br/feed/openbaypro.php
+++ admin/language/portuguese-br/feed/openbaypro.php
@@ -3,5 +3,5 @@
 $_['heading_title']	  = 'OpenBay Pro';
 
 // Text
-$_['text_module']    = 'Moduleos';
+$_['text_module']    = 'Modulos';
 $_['text_installed'] = 'O módulo OpenBay Pro já está instalado. Ele está disponível em Extenções -> OpenBay Pro';
