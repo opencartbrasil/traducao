29b8310385c2577dcd1a8e984e461f3957c1facb
elitontorri <falecom@elitontorri.com.br>
2014-11-08 15:13:39 -0200

    Update search.php

--- catalog/language/portuguese-br/product/search.php
+++ catalog/language/portuguese-br/product/search.php
@@ -1,34 +1,34 @@
 <?php
 // Heading
-$_['heading_title']     = 'Search';
+$_['heading_title']     = 'Busca';
 $_['heading_tag']		= 'Tag - ';
 
 // Text
-$_['text_search']       = 'Products meeting the search criteria';
-$_['text_keyword']      = 'Keywords';
-$_['text_category']     = 'All Categories';
-$_['text_sub_category'] = 'Search in subcategories';
-$_['text_empty']        = 'There is no product that matches the search criteria.';
-$_['text_quantity']     = 'Qty:';
-$_['text_manufacturer'] = 'Brand:';
-$_['text_model']        = 'Product Code:';
-$_['text_points']       = 'Reward Points:';
-$_['text_price']        = 'Price:';
-$_['text_tax']          = 'Ex Tax:';
-$_['text_reviews']      = 'Based on %s reviews.';
-$_['text_compare']      = 'Product Compare (%s)';
-$_['text_sort']         = 'Sort By:';
-$_['text_default']      = 'Default';
-$_['text_name_asc']     = 'Name (A - Z)';
-$_['text_name_desc']    = 'Name (Z - A)';
-$_['text_price_asc']    = 'Price (Low &gt; High)';
-$_['text_price_desc']   = 'Price (High &gt; Low)';
-$_['text_rating_asc']   = 'Rating (Lowest)';
-$_['text_rating_desc']  = 'Rating (Highest)';
-$_['text_model_asc']    = 'Model (A - Z)';
-$_['text_model_desc']   = 'Model (Z - A)';
-$_['text_limit']        = 'Show:';
+$_['text_search']       = 'Produtos que cumprem os critérios da busca';
+$_['text_keyword']      = 'Palavras-chave';
+$_['text_category']     = 'Todos os Departamentos';
+$_['text_sub_category'] = 'Procurar nos subdepartamentos';
+$_['text_empty']        = 'Não há nenhum produto que corresponda aos critérios da busca.';
+$_['text_quantity']     = 'Qtd:';
+$_['text_manufacturer'] = 'Fabricante:';
+$_['text_model']        = 'Código do Produto:';
+$_['text_points']       = 'Pontos pela Compra:';
+$_['text_price']        = 'Preço:';
+$_['text_tax']          = 'Taxa:';
+$_['text_reviews']      = 'Baseado em %s comentários.';
+$_['text_compare']      = 'Comparar Produtos (%s)';
+$_['text_sort']         = 'Ordenar por:';
+$_['text_default']      = 'Padrão';
+$_['text_name_asc']     = 'Nome (A - Z)';
+$_['text_name_desc']    = 'Nome (Z - A)';
+$_['text_price_asc']    = 'Preço (Menor &gt; Maior)';
+$_['text_price_desc']   = 'Preço (Maior &gt; Menor)';
+$_['text_rating_asc']   = 'Avaliação (Baixa)';
+$_['text_rating_desc']  = 'Avaliação (Alta)';
+$_['text_model_asc']    = 'Modelo (A - Z)';
+$_['text_model_desc']   = 'Modelo (Z - A)';
+$_['text_limit']        = 'Exibir até:';
 
 // Entry
-$_['entry_search']      = 'Search Criteria';
-$_['entry_description'] = 'Search in product descriptions';
+$_['entry_search']      = 'Busca';
+$_['entry_description'] = 'Buscar nas descrições dos produtos';
