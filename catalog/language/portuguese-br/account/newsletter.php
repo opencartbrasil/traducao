51c35fd2ca4df8a5e1b5319bca8302468f0a0aa1
Jano Sousa <sollykma@gmail.com>
2014-11-07 23:16:25 -0200

    Tradução newsletter.php
    Arquivo newsletter.php traduzido!

--- catalog/language/portuguese-br/account/newsletter.php
+++ catalog/language/portuguese-br/account/newsletter.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title']    = 'Newsletter Subscription';
+$_['heading_title']    = 'Meu Informativo';
 
 // Text
-$_['text_account']     = 'Account';
-$_['text_newsletter']  = 'Newsletter';
-$_['text_success']     = 'Success: Your newsletter subscription has been successfully updated!';
+$_['text_account']     = 'Minha Conta';
+$_['text_newsletter']  = 'Meu Informativo';
+$_['text_success']     = 'Inscrição no informativo atualizada com sucesso!';
 
 // Entry
-$_['entry_newsletter'] = 'Subscribe';
+$_['entry_newsletter'] = 'Receber o Informativo';
