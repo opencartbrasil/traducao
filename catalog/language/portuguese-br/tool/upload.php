bdadd227e7c1ce35d3183573d4b97b0526d0b66a
elitontorri <falecom@elitontorri.com.br>
2014-11-08 13:43:08 -0200

    Update upload.php

--- catalog/language/portuguese-br/tool/upload.php
+++ catalog/language/portuguese-br/tool/upload.php
@@ -1,8 +1,8 @@
 <?php
 // Text
-$_['text_upload']    = 'Your file was successfully uploaded!';
+$_['text_upload']    = 'Seu arquivo foi enviado com sucesso!';
 
 // Error
-$_['error_filename'] = 'Filename must be between 3 and 64 characters!';
-$_['error_filetype'] = 'Invalid file type!';
-$_['error_upload']   = 'Upload required!';
+$_['error_filename'] = 'O nome do arquivo deve estar entre 3 e 64 caracteres!';
+$_['error_filetype'] = 'Tipo de arquivo inválido!';
+$_['error_upload']   = 'Upload necessário!';
