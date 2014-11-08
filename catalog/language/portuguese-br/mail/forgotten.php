d78ef9e0022d9a3e5ed464e7c6668cb1d12bc31d
elitontorri <falecom@elitontorri.com.br>
2014-11-08 15:23:13 -0200

    Update forgotten.php

--- catalog/language/portuguese-br/mail/forgotten.php
+++ catalog/language/portuguese-br/mail/forgotten.php
@@ -1,5 +1,5 @@
 <?php
 // Text
-$_['text_subject']  = '%s - New Password';
-$_['text_greeting'] = 'A new password was requested from %s.';
-$_['text_password'] = 'Your new password is:';
+$_['text_subject']  = '%s - Nova Senha';
+$_['text_greeting'] = 'Uma nova senha foi solicitada através da loja %s.';
+$_['text_password'] = 'Sua nova senha é:';
