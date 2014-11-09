a73637a9a3f83d14988f5d49f71d4b4709a16572
Eliton Torri <falecom@elitontorri.com.br>
2014-11-09 09:19:14 -0200

    Update item.php

--- admin/language/portuguese-br/shipping/item.php
+++ admin/language/portuguese-br/shipping/item.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']     = 'Per Item';
+$_['heading_title']     = 'Frete por Itens';
 
 // Text
-$_['text_shipping']    = 'Shipping';
-$_['text_success']     = 'Success: You have modified shipping per item rates!';
-$_['text_edit']        = 'Edit Per Item Shipping';
+$_['text_shipping']    = 'Formas de Envio';
+$_['text_success']     = 'Módulo Frete por Itens modificado com sucesso!';
+$_['text_edit']        = 'Editar Frete por Itens';
 
 // Entry
-$_['entry_cost']       = 'Cost';
-$_['entry_tax_class']  = 'Tax Class';
-$_['entry_geo_zone']   = 'Geo Zone';
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_cost']       = 'Valor por Item';
+$_['entry_tax_class']  = 'Grupo de Impostos';
+$_['entry_geo_zone']   = 'Região Geográfica';
+$_['entry_status']     = 'Situação';
+$_['entry_sort_order'] = 'Ordem de Exibição';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify shipping per item rates!';
+$_['error_permission'] = 'Atenção: Você não possui permissão para modificar o módulo Frete por Itens!';
