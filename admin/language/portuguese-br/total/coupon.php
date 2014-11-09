a0536c67fa569a748e68bbb015c9081a6525582d
Eliton Torri <falecom@elitontorri.com.br>
2014-11-09 00:47:27 -0200

    Update coupon.php

--- admin/language/portuguese-br/total/coupon.php
+++ admin/language/portuguese-br/total/coupon.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']    = 'Coupon';
+$_['heading_title']    = 'Cupom';
 
 // Text
-$_['text_total']       = 'Order Totals';
-$_['text_success']     = 'Success: You have modified coupon total!';
-$_['text_edit']        = 'Edit Coupon';
+$_['text_total']       = 'Finalização do Pedido';
+$_['text_success']     = 'Módulo Cupom modificado com sucesso';
+$_['text_edit']        = 'Editar Cupom';
 
 // Entry
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_status']     = 'Situação';
+$_['entry_sort_order'] = 'Ordem de Exibição';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify coupon total!';
+$_['error_permission'] = 'Atenção: Você não possui permissão para modificar o módulo Cupom!';
