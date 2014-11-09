ed150d6d0064d56036518ed125f0dd76ca13e457
Eliton Torri <falecom@elitontorri.com.br>
2014-11-09 09:44:48 -0200

    Update amazon_button.php

--- admin/language/portuguese-br/module/amazon_button.php
+++ admin/language/portuguese-br/module/amazon_button.php
@@ -2,12 +2,12 @@
 // Heading
 $_['heading_title']    = 'Amazon Payments Button';
 
-$_['text_module']      = 'Modules';
-$_['text_success']     = 'Success: You have modified Amazon payments module!';
-$_['text_edit']        = 'Edit Amazon Payments Module';
+$_['text_module']      = 'Módulos';
+$_['text_success']     = 'Módulo Amazon Payments Button modificado com sucesso!';
+$_['text_edit']        = 'Editar Amazon Payments Module';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Situação';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify Amazon payments module!';
+$_['error_permission'] = 'Atenção: Você não possui permissão para modificar o módulo Amazon payments';
