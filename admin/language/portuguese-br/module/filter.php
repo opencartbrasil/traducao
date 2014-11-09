67fa752bd1051e7cde269b2ef166108b027a22b8
Eliton Torri <falecom@elitontorri.com.br>
2014-11-09 10:11:31 -0200

    Update filter.php

--- admin/language/portuguese-br/module/filter.php
+++ admin/language/portuguese-br/module/filter.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']    = 'Filter';
+$_['heading_title']    = 'Filtros';
 
 // Text
-$_['text_module']      = 'Modules';
-$_['text_success']     = 'Success: You have modified filter module!';
-$_['text_edit']        = 'Edit Filter Module';
+$_['text_module']      = 'Módulos';
+$_['text_success']     = 'Módulo Filtros modificado com sucesso!';
+$_['text_edit']        = 'Editar Filtros';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Situação';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify filter module!';
+$_['error_permission'] = 'Atenção: Você não possui permissão para modificar o módulo Filtros!';
