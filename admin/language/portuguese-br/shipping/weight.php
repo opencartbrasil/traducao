52e76ecb4dc07516dfcd788a8e8043d095f7547a
Eliton Torri <falecom@elitontorri.com.br>
2014-11-09 09:27:38 -0200

    Update weight.php

--- admin/language/portuguese-br/shipping/weight.php
+++ admin/language/portuguese-br/shipping/weight.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']    = 'Weight Based Shipping';
+$_['heading_title']    = 'Frete por Peso';
 
 // Text
-$_['text_shipping']    = 'Shipping';
-$_['text_success']     = 'Success: You have modified weight based shipping!';
-$_['text_edit']        = 'Edit Weight Based Shipping';
+$_['text_shipping']    = 'Formas de Envio';
+$_['text_success']     = 'Módulo Frete por Peso modificado com sucesso!';
+$_['text_edit']        = 'Editar Frete por Peso';
 
 // Entry
-$_['entry_rate']       = 'Rates';
-$_['entry_tax_class']  = 'Tax Class';
-$_['entry_geo_zone']   = 'Geo Zone';
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_rate']       = 'Pesos e Valores';
+$_['entry_tax_class']  = 'Grupo de Impostos';
+$_['entry_geo_zone']   = 'Região Geográfica';
+$_['entry_status']     = 'Situação';
+$_['entry_sort_order'] = 'Ordem de Exibição';
 
 // Help
-$_['help_rate']        = 'Example: 5:10.00,7:12.00 Weight:Cost,Weight:Cost, etc..';
+$_['help_rate']        = 'Peso:Valor,Peso:Valor,etc.<br />Supondo que a unidade de peso padrãoo é o Quilograma: <strong>0.3:10.00,1:12.00,2:15.00</strong><br />Ou seja, até 300 gramas R$ 10,00, até 1 kg R$ 12,00, até 2 kg R$ 15,00.';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify weight based shipping!';
+$_['error_permission'] = 'Atenção: Você não possui permissão para modificar o módulo Frete por Peso!';
