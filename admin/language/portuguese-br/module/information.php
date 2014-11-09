25d3bfc9a91d2c5cf3671ab4afe02bbeb7dc29fd
Eliton Torri <falecom@elitontorri.com.br>
2014-11-09 10:55:57 -0200

    Update information.php

--- admin/language/portuguese-br/module/information.php
+++ admin/language/portuguese-br/module/information.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']    = 'Information';
+$_['heading_title']    = 'Menu de Páginas de Informações';
 
 // Text
-$_['text_module']      = 'Modules';
-$_['text_success']     = 'Success: You have modified information module!';
-$_['text_edit']        = 'Edit Information Module';
+$_['text_module']      = 'Módulos';
+$_['text_success']     = 'Módulo Menu de Páginas de Informações modificado com sucesso!';
+$_['text_edit']        = 'Editar Menu de Páginas de Informações';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Situação';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify information module!';
+$_['error_permission'] = 'Atenção: Você não possui permissão para modificar o módulo Páginas de Informações!';
