75edae5ffe3c0acb9fd6d98575d604f35f476fdb
Eliton Torri <falecom@elitontorri.com.br>
2014-11-09 10:03:00 -0200

    Update category.php

--- admin/language/portuguese-br/module/category.php
+++ admin/language/portuguese-br/module/category.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']    = 'Category';
+$_['heading_title']    = 'Menu Departamentos';
 
 // Text
-$_['text_module']      = 'Modules';
-$_['text_success']     = 'Success: You have modified category module!';
-$_['text_edit']        = 'Edit Category Module';
+$_['text_module']      = 'Módulos';
+$_['text_success']     = 'Módulo Menu Departamentos modificado com sucesso';
+$_['text_edit']        = 'Editar Menu Departamentos';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Situação';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify category module!';
+$_['error_permission'] = 'Atenção: Você não possui permissão para modificar o módulo Menu Departamentos!';
