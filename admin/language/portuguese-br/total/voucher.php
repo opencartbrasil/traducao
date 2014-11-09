1068bff96be34c5fbd5dba0604cb6a1b84bdc35f
Eliton Torri <falecom@elitontorri.com.br>
2014-11-09 00:53:26 -0200

    Update voucher.php

--- admin/language/portuguese-br/total/voucher.php
+++ admin/language/portuguese-br/total/voucher.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']    = 'Gift Voucher';
+$_['heading_title']    = 'Vale Presentes';
 
 // Text
-$_['text_total']       = 'Order Totals';
-$_['text_success']     = 'Success: You have modified gift voucher total!';
-$_['text_edit']        = 'Edit Gift Voucher Total';
+$_['text_total']       = 'Finalização do Pedido';
+$_['text_success']     = 'Módulo Vale Presentes modificado com sucesso';
+$_['text_edit']        = 'Editar Vale Presentes';
 
 // Entry
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_status']     = 'Situação';
+$_['entry_sort_order'] = 'Ordem de Exibição';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify gift voucher total!';
+$_['error_permission'] = 'Atenção: Você não possui permissão para modificar o módulo Vale Presentes!';
