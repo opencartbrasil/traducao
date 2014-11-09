362ec6349d858809b2b123945341156061c972f8
Eliton Torri <falecom@elitontorri.com.br>
2014-11-09 10:01:20 -0200

    Update carousel.php

--- admin/language/portuguese-br/module/carousel.php
+++ admin/language/portuguese-br/module/carousel.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']    = 'Carousel';
+$_['heading_title']    = 'Carrossel de Imagens';
 
 // Text
-$_['text_module']      = 'Modules';
-$_['text_success']     = 'Success: You have modified carousel module!';
-$_['text_edit']        = 'Edit Carousel Module';
+$_['text_module']      = 'Módulos';
+$_['text_success']     = 'Módulo Carrossel de Imagens modificado com sucesso!';
+$_['text_edit']        = 'Edita Carrossel de Imagens';
 
 // Entry
 $_['entry_banner']     = 'Banner';
-$_['entry_limit']      = 'Limit';
-$_['entry_scroll']     = 'Scroll';
-$_['entry_image']      = 'Image (W x H) and Resize Type';
-$_['entry_width']      = 'Width';
-$_['entry_height']     = 'Height';
-$_['entry_status']     = 'Status';
+$_['entry_limit']      = 'Limite';
+$_['entry_scroll']     = 'Rolar';
+$_['entry_image']      = 'Imagem (L x A)';
+$_['entry_width']      = 'Largura';
+$_['entry_height']     = 'Altura';
+$_['entry_status']     = 'Situação';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify carousel module!';
-$_['error_module']     = 'Warning: Module required!';
-$_['error_image']      = 'Image width &amp; height dimensions required!';
+$_['error_permission'] = 'Atenção: Você não possui permissão para modificar o módulo Carrossel de Imagens!';
+$_['error_module']     = 'Atenção: Módulo necessário!';
+$_['error_image']      = 'Largura &amp; Altura Dimensões necessárias!';
