69e61d9e9568af312cd4a35066602c8a0888cfd2
Eliton Torri <falecom@elitontorri.com.br>
2014-11-09 10:37:01 -0200

    Update google_hangouts.php

--- admin/language/portuguese-br/module/google_hangouts.php
+++ admin/language/portuguese-br/module/google_hangouts.php
@@ -3,17 +3,17 @@
 $_['heading_title']    = 'Google Hangouts';
 
 // Text
-$_['text_module']      = 'Modules';
-$_['text_success']     = 'Success: You have modified Google Hangouts module!';
-$_['text_edit']        = 'Edit Google Hangouts Module';
+$_['text_module']      = 'Módulos';
+$_['text_success']     = 'Módulo Google Hangouts modificado com sucesso!';
+$_['text_edit']        = 'Editar Google Hangouts';
 
 // Entry
-$_['entry_code']       = 'Google Talk Code';
-$_['entry_status']     = 'Status';
+$_['entry_code']       = 'Código Google Talk';
+$_['entry_status']     = 'Situação';
 
 // Help
-$_['help_code']        = 'Goto <a href="https://developers.google.com/+/hangouts/button" target="_blank">Create a Google Hangout chatback badge</a> and copy &amp; paste the generated code into the text box.';
+$_['help_code']        = 'Acesse <a href="https://developers.google.com/+/hangouts/button" target="_blank">Crie um Google Hangout,</a> copie e cole o código gerado na caixa de texto na página que vai ser carregada.';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify Google Hangouts module!';
-$_['error_code']       = 'Code Required';
+$_['error_permission'] = 'Atenção: Você não possui permissão para modificar o módulo Google Hangouts!';
+$_['error_code']       = 'Código Necessário';
