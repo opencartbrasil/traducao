20b11ba2b7645bea66cbfe2f9e310c9ca1749392
Manoel Vidal <manoel.vidal@opencartbrasil.com.br>
2014-12-07 14:56:00 -0300

    Melhorias.

--- install.php
+++ install.php
@@ -2,36 +2,44 @@
 $this->load->model('localisation/language');
 $results = $this->model_localisation_language->getLanguages();
 
+$iLang = 1;
+
 foreach ($results as $result) {
 	if ($result['code'] == 'pt-br') {
-		$this->model_localisation_language->deleteLanguage($result['language_id']);
+		$iLang = 0;
 	}
 }
 
-$oc_language['name']       = 'Português (BR)';
-$oc_language['code']       = 'pt-br';
-$oc_language['locale']     = 'pt_BR.UTF-8, pt_BR, UTF-8';
-$oc_language['directory']  = 'portuguese-br';
-$oc_language['image']      = 'br.png';
-$oc_language['sort_order'] = 1;
-$oc_language['status']     = 1;
-$this->model_localisation_language->addLanguage($oc_language);
+if ($iLang) {
+	$oc_language['name']       = 'Português (BR)';
+	$oc_language['code']       = 'pt-br';
+	$oc_language['locale']     = 'pt_BR.UTF-8, pt_BR, UTF-8';
+	$oc_language['directory']  = 'portuguese-br';
+	$oc_language['image']      = 'br.png';
+	$oc_language['sort_order'] = 1;
+	$oc_language['status']     = 1;
+	$this->model_localisation_language->addLanguage($oc_language);
+}
 
 $this->load->model('localisation/currency');
 $results = $this->model_localisation_currency->getCurrencies();
 
+$iCurr = 1;
+
 foreach ($results as $result) {
 	if ($result['code'] == 'BRL') {
-		$this->model_localisation_currency->deleteCurrency($result['currency_id']);
+		$iCurr = 0;
 	}
 }
 
-$oc_currency['title']         = 'Real';
-$oc_currency['code']          = 'BRL';
-$oc_currency['symbol_left']   = 'R$';
-$oc_currency['symbol_right']  = '';
-$oc_currency['decimal_place'] = '2';
-$oc_currency['value']         = 1.00000000;
-$oc_currency['status']        = 1;
+if ($iCurr) {
+	$oc_currency['title']         = 'Real';
+	$oc_currency['code']          = 'BRL';
+	$oc_currency['symbol_left']   = 'R$';
+	$oc_currency['symbol_right']  = '';
+	$oc_currency['decimal_place'] = '2';
+	$oc_currency['value']         = 1.00000000;
+	$oc_currency['status']        = 1;
 
-$this->model_localisation_currency->addCurrency($oc_currency);
+	$this->model_localisation_currency->addCurrency($oc_currency);
+}
