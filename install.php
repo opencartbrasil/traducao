a898ed5b3293394873469341147146f9125b1492
David CHC <david.chc@gmail.com>
2016-11-14 17:39:57 -0200

    Define o idioma para ingles se for executado o install.sql antes do install.php

--- install.php
+++ install.php
@@ -16,6 +16,7 @@ if ($iLang) {
   $oc_language['locale']     = 'pt_BR.UTF-8,pt_BR,pt-br,pt_br,portuguese';
   $oc_language['sort_order'] = 1;
   $oc_language['status']     = 1;
+  $this->config->set('config_language_id', 1);
   $this->model_localisation_language->addLanguage($oc_language);
 }
 
